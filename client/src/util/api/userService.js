621e16db440a9c1ee20eeb45973a61f4a5a1b737
HaoThien <tuhaothien52@gmail.com>
2024-12-16 17:54:34 +0700

    Update userService.js

--- client/src/util/api/userService.js
+++ client/src/util/api/userService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://deploy-pp57.onrender.com//api'; // Backend API URL
+const API_BASE_URL = 'https://deploy-pp57.onrender.com/api'; // Backend API URL
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
